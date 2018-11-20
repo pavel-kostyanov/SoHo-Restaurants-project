8680b51f94a1ec648912c7fa958759f95bba18b5
pavel-kostyanov <kspavel777@gmail.com>
2018-11-20 18:43:37 +0300

    draft

--- trie.py
+++ trie.py
@@ -9,7 +9,7 @@ class Trie_Node():
     def __init__(self, letter = 0):
         self.letter = letter
         self.children = {}
-        self.words_end = False
+        self.word_end = False
         
     
 
@@ -21,16 +21,25 @@ class Trie():
     def insert_word(self, word):
         node = self.root_node
         for i in range(len(word)):
-            current_letter = word(i)
+            current_letter = word[i]
             if node.children[current_letter]:
                 node = node.children[current_letter]
             else:    
-                new_node = Node(current_letter)
+                new_node = Trie_Node(current_letter)
+                node.children[current_letter] = new_node
                 node = new_node
+        node.word_end = True        
         
     
     def find_word(self, word):
-        pass
+        node = self.root_node
+        for i in range(len(word)):
+            current_letter = word[i]
+            if node.children[current_letter]:
+                node = node.children[current_letter] 
+            else:
+                return False
+        return True     
     
     def remove_word(self, word):
         pass
