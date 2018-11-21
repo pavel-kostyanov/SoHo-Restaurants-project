2cfd53b793053c5794b2c1f79a764b998eee8bf3
pavel-kostyanov <kspavel777@gmail.com>
2018-11-21 18:42:35 +0300

    files data script added

--- trie.py
+++ trie.py
@@ -10,37 +10,55 @@ class Trie_Node():
         self.letter = letter
         self.children = {}
         self.word_end = False
-        
-    
+
+    def get_letter(self):
+        return self.letter
 
 class Trie():
     def __init__(self):
         self.root_node = Trie_Node()
-         
-    
+
+    def get_root_node(self):
+        return self.root_node
+
     def insert_word(self, word):
         node = self.root_node
         for i in range(len(word)):
             current_letter = word[i]
-            if node.children[current_letter]:
+            if current_letter in node.children:
                 node = node.children[current_letter]
-            else:    
+            else:
                 new_node = Trie_Node(current_letter)
                 node.children[current_letter] = new_node
                 node = new_node
-        node.word_end = True        
-        
-    
+        node.word_end = True
+
+
     def find_word(self, word):
         node = self.root_node
         for i in range(len(word)):
             current_letter = word[i]
             if node.children[current_letter]:
-                node = node.children[current_letter] 
+                node = node.children[current_letter]
             else:
                 return False
-        return True     
-    
+        return True
+
+    def find_word_by_prefix(self, prefix):
+        node = self.root_node
+        word = ''
+        for i in range(len(prefix)):
+            current_letter = prefix[i]
+            if prefix[i] in node.children:
+                node = node.children[current_letter]
+                while node.word_end == False:
+                    ltr = node.get_letter()
+                    print(node.children)
+                    node = node.children[ltr]
+            else:
+                return False
+        print(word)
+        return True
+
     def remove_word(self, word):
         pass
-    
