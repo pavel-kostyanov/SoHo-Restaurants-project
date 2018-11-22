8691c8de6a0f4a3589ec4972b7a2c5063c732479
pavel-kostyanov <kspavel777@gmail.com>
2018-11-23 00:03:59 +0300

    draft

--- script.py
+++ script.py
@@ -1,27 +1,28 @@
-from trie import Trie, Trie_Node
+from trie import *
 from data import *
 from welcome import *
-#from hashmap import HashMap
-#from linkedlist import LinkedList
+# from hash map import HashMap
+# from linked list import LinkedList
 
-#Printing the Welcome Message
+# Printing the Welcome Message
 print_welcome()
 
-#Write code to insert food types into a data structure here. The data is in data.py
+# Write code to insert food types into a data structure here. The data is in data.py
 food_trie = Trie()
 for i in range(len(types)):
     food_trie.insert_word(types[i])
-    #print(types[i] + '-' + '\n')
+    # print(types[i] + '-' + '\n')
 
-rootnode = food_trie.get_root_node()
+root_node = food_trie.get_root_node()
 
-#Write code to insert restaurant data into a data structure here. The data is in data.py
+# Write code to insert restaurant data into a data structure here. The data is in data.py
 
 
-#Write code for user interaction here
+# Write code for user interaction here
 while True:
-    user_input = str(input("\nWhat type of food would you like to eat?\nType the beginning of that food type and press enter to see if it's here.\n")).lower()
-    #Search for user_input in food types data structure here
+    user_input = str(input("\nWhat type of food would you like to eat?\nType the beginning of that food type and press \
+     enter to see if it's here.\n")).lower()
+    # Search for user_input in food types data structure here
     food_trie.find_word_by_prefix(user_input)
 
-    #After finding food type write code for retrieving restaurant data here
+    # After finding food type write code for retrieving restaurant data h
--- trie.py
+++ trie.py
@@ -5,18 +5,20 @@ Created on Tue Nov 20 14:53:41 2018
 @author: pavel
 """
 
-class Trie_Node():
-    def __init__(self, letter = 0):
+
+class TrieNode:
+    def __init__(self, letter = None, word_end = False):
         self.letter = letter
         self.children = {}
-        self.word_end = False
+        self.word_end = word_end
 
     def get_letter(self):
         return self.letter
 
-class Trie():
+
+class Trie:
     def __init__(self):
-        self.root_node = Trie_Node()
+        self.root_node = TrieNode()
 
     def get_root_node(self):
         return self.root_node
@@ -28,11 +30,11 @@ class Trie():
             if current_letter in node.children:
                 node = node.children[current_letter]
             else:
-                new_node = Trie_Node(current_letter)
+                new_node = TrieNode(current_letter)
                 node.children[current_letter] = new_node
                 node = new_node
-        node.word_end = True
-
+        node = TrieNode(letter = word, word_end = True)
+        # node.word_end = True
 
     def find_word(self, word):
         node = self.root_node
@@ -44,20 +46,54 @@ class Trie():
                 return False
         return True
 
+    def tree_search(self, node):
+        if node.word_end == True:
+            return
+
+        for letter in node.children.keys():
+            if node.word_end == False:
+                word = letter
+                node = node.children[letter]
+                if node.word_end == False:
+                    for letter in node.children.keys():
+                        word = letter
+                        node = node.children[letter]
+                        if node.word_end == False:
+                            for letter in node.children.keys():
+                                word = letter
+                                node = node.children[letter]
+                                if node.word_end == False:
+                                    for letter in node.children.keys():
+                                        word = letter
+                                        node = node.children[letter]
+                                else:
+                                    return node.letter
+                # return self.tree_search(node)
+            else:
+                return word
+
+
     def find_word_by_prefix(self, prefix):
         node = self.root_node
         word = ''
+        current_letter = None
         for i in range(len(prefix)):
             current_letter = prefix[i]
-            if prefix[i] in node.children:
+            if current_letter in node.children:
                 node = node.children[current_letter]
-                while node.word_end == False:
-                    ltr = node.get_letter()
-                    print(node.children)
-                    node = node.children[ltr]
+                word += current_letter
             else:
                 return False
+        print(node.children)
+        if node.children:
+            x = self.tree_search(node)
+            print(x)
+            
+        else:
+            return
+
         print(word)
+
         return True
 
     def remove_word(self, word):
