43da94399752d4f72d0cc7dd284315a158839e1e
pavel-kostyanov <kspavel777@gmail.com>
2018-11-28 00:03:51 +0300

    trying to create search by prefix with a loop

--- trie.py
+++ trie.py
@@ -7,10 +7,11 @@ Created on Tue Nov 20 14:53:41 2018
 
 
 class TrieNode:
-    def __init__(self, letter = None, word_end = False):
+    def __init__(self, letter = None, value = None, word_end = False):
         self.letter = letter
-        self.children = {}
+        self.value = value
         self.word_end = word_end
+        self.children = {}
 
     def get_letter(self):
         return self.letter
@@ -23,6 +24,30 @@ class Trie:
     def get_root_node(self):
         return self.root_node
 
+    def printAllNodes(self):
+        node = self.get_root_node
+        for letter in node.children.key:
+            print(letter)
+            node = node.children(letter)
+            for letter in node.children.key:
+                print(letter)
+                node = node.children(letter)
+
+
+    def insert_word(self, word):
+        node = self.get_root_node
+        for i in range(len(word)):
+            current_letter = word[i]
+            if current_letter in node.children:
+                node = node.children[current_letter]
+            else:
+                new_node = TrieNode(letter = current_letter)
+                node.children[word[i]] = new_node
+                node = new_node
+         node.value = word
+
+
+"""
     def insert_word(self, word):
         node = self.root_node
         for i in range(len(word)):
@@ -35,6 +60,7 @@ class Trie:
                 node = new_node
         node = TrieNode(letter = word, word_end = True)
         # node.word_end = True
+"""
 
     def find_word(self, word):
         node = self.root_node
@@ -75,20 +101,38 @@ class Trie:
 
     def find_word_by_prefix(self, prefix):
         node = self.root_node
+        variants = []
         word = ''
         current_letter = None
         for i in range(len(prefix)):
             current_letter = prefix[i]
             if current_letter in node.children:
                 node = node.children[current_letter]
-                word += current_letter
-            else:
-                return False
+        while node.children:
+            for key in node.children.keys():
+                next_node = node.children[key]
+                if next_node.value:
+                    variants.append(value)
+                if not next_node.children:
+                    return
+                else:
+                    for key in next_node.children.keys():
+                        next_node = next_node.children[key]
+                        if next_node.value:
+                            variants.append(value)
+                        if not next_node.children:
+                            return
+                        else:
+                            for key in next_node.children.keys():
+                                next_node = next_node.children[key]    
+
+
+
         print(node.children)
         if node.children:
             x = self.tree_search(node)
             print(x)
-            
+
         else:
             return
 
