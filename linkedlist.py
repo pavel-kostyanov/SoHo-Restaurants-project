a88483503e1b13c6c0fe420e34d181561db5fbae
pavel-kostyanov <kspavel777@gmail.com>
2018-12-04 00:06:07 +0300

    fully working prodaction version 1.0

--- linkedlist.py
+++ linkedlist.py
@@ -33,11 +33,15 @@ class LinkedList:
         else:
           current_node = next_node
 
-  def stringify_list_with_proper_keys_only(self, key):
-    string_list = "---------------------\n\n"
+  def get_restaurants_data(self, key):
+    string_list = '---------------------\n\n'
     current_node = self.head_node
     while current_node:
       if current_node.value[0] == key:
-        string_list += "Name: {restaraunts_info[0]}\nPrice: {restaraunts_info[1]}\nRating: {restaraunts_info[2]}\nAddress: {restaraunts_info[3]}\n\n---------------------\n\n".format(restaraunts_info = current_node.value[1])
+        string_list += ('Name: {restaraunts_info[0]}\n'
+                        'Price: {restaraunts_info[1]}\n'
+                        'Rating: {restaraunts_info[2]}\n'
+                        'Address: {restaraunts_info[3]}\n\n'
+                        '---------------------\n\n'.format(restaraunts_info = current_node.value[1]))
       current_node = current_node.get_next_node()
     return string_list
--- script.py
+++ script.py
@@ -2,7 +2,7 @@ from trie import *
 from data import *
 from welcome import *
 from hashmap import HashMap
-from linkedlist import LinkedList
+
 
 # Printing the Welcome Message
 print_welcome()
@@ -10,14 +10,13 @@ print_welcome()
 # Write code to insert food types into a data structure here. The data is in data.py
 # We create an instance of the Trie class
 food_trie = Trie()
-for i in range(len(types)): # We fill in the Trie with types of food from 'types' list from data.py
+for i in range(len(types)):  # We fill in the Trie with types of food from 'types' list from data.py
     food_trie.insert_word(types[i])
-
 
 # Write code to insert restaurant data into a data structure here. The data is in data.py
 # We create an instance of the HashMap class
 restaurant_hash_table = HashMap(len(types))
-for i in restaurant_data:# We create the key-value pairs using the restaurant_data list from data.py
+for i in restaurant_data:  # We create the key-value pairs using the restaurant_data list from data.py
     restaurant_hash_table.assign(i[0], i[1:])
 
 
@@ -29,25 +28,48 @@ while True:
     food_type = food_trie.find_word_by_prefix(user_input)
 
     # After finding food type write code for retrieving restaurant data h
-    if food_type == None: # if user enter letter that is not in database
+    if not food_type:  # if user enter letter that is not in database
         print("The type of food starting with this letters is not in our database")
 
-    if isinstance(food_type, list) and len(food_type) == 1: # if user enters
-        user_input = str(input("The only option with those letters is {0}. Do you want to look at {0} restaurants? \n Enter \"y\" for YES and \"n\" for NO. - ".format(food_type[0]))).lower()
-        while (user_input != "y" and user_input != "n"):
+    if isinstance(food_type, list) and len(food_type) == 1:  # if user enters
+        user_input = str(input('The only option with those letters is {0}. Do you want to look at {0} restaurants? \n'
+                               'Enter \"y\" for YES and \"n\" for NO. - '.format(food_type[0]))).lower()
+        while user_input != "y" and user_input != "n":
             print("\nEnter only \"y\" for YES and \"n\" for NO.")
-            user_input = input("Enter again - ")
+            user_input = input("Enter again - ").lower()
         if user_input == "y":
             print("\nThe {} Restaurants in SoHo are... ".format(food_type[0].title()))
             list_of_all_restaurants = restaurant_hash_table.retrieve(food_type[0])
-            print(list_of_all_restaurants.stringify_list_with_proper_keys_only(food_type[0]))
+            print(list_of_all_restaurants.get_restaurants_data(food_type[0]))
+            user_input = input(
+                "Do you want to find other restaurants? Enter \"y\" for YES and \"n\" for NO. - ").lower()
+            while user_input != "y" and user_input != "n":
+                print("\nEnter only \"y\" for YES and \"n\" for NO.")
+                user_input = input("Enter again - ").lower()
+            if user_input == 'y':
+                continue
+            if user_input == 'n':
+                break
         if user_input == "n":
             continue
 
-    if isinstance(food_type, list) and  len(food_type) > 1: # this is the case when we are getting more than just one result from a type_food array
+    # this is the case when we are getting more than just one result from a type_food array
+    if isinstance(food_type, list) and len(food_type) > 1:
         print("With those beginning letters, your choices are {} ".format(', '.join(food_type)))
 
-    if type(food_type) == str: # this is the case when a user enters correct and whole type of food, so we just handle a user input as a string
+    # this is the case when a user enters correct and whole type of food, so we just handle a user input as a string
+    if type(food_type) == str:
         print("\nThe {} Restaurants in SoHo are... ".format(food_type.title()))
+        # variable  "list_of_all_restaurants" is a linked list of all restaurants from hash-table
         list_of_all_restaurants = restaurant_hash_table.retrieve(food_type)
-        print(list_of_all_restaurants.stringify_list_with_proper_keys_only(food_type))
+        # variable  "list_of_all_restaurants" is transformed into formatted text onto the screen
+        print(list_of_all_restaurants.get_restaurants_data(food_type))
+        user_input = input("Do you want to find other restaurants? Enter \"y\" for YES and \"n\" for NO. - ").lower()
+        #  we forced a user enter Y or N only
+        while user_input != "y" and user_input != "n":
+            print("\nEnter only \"y\" for YES and \"n\" for NO.")
+            user_input = input("Enter again - ").lower()
+        if user_input == 'y':
+            continue
+        if user_input == 'n':
+            break
